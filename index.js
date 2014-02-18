f3d900536b298c2da2df674e78afe343c94b5d80
Jarrett Cruger <jcrugzz@gmail.com>
2014-02-17 23:24:20 -0500

    [api] return null if no url is passed in rather than throwing

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var parse = require('url').parse;
 // with inserted auth. Prevent unnecessary parsing just because
 //
 module.exports = function (url, auth) {
+  if (!url) {
+    return null;
+  }
   var isString = typeof url == 'string';
   var parsed = isString ? parse(url) : url;
   var str = parsed.href;
